cf16a5c4eeb3cfca4b9e05530ac4b17ce976b71c
kkkkCampbell <memberof@gmail.com>
2026-09-18 10:19:32 +0300

    Update malw_parser.sh

--- malw_parser.sh
+++ malw_parser.sh
@@ -1,4 +1,5 @@
 clear
+echo ""
 echo "Устанавливается парсер hosts-файла dns.malw.link"
 echo ""
 
@@ -11,6 +12,12 @@ HOSTSFILE="/root/malw_parser/malwhosts.txt"
 wget -qO- "https://raw.githubusercontent.com/ImMALWARE/dns.malw.link/master/hosts" \
   | awk '!/^#/ && NF >= 2 { for (i = 2; i <= NF; i++) if ($i ~ /[a-zA-Z]/) print $i }' \
   > ${HOSTSFILE}
+
+if [ -t 1 ]; then
+    printf 'Файл с доменами расположен в \033[33m%s\033[0m\n' "$HOSTSFILE"
+else
+    printf 'Файл с доменами расположен в %s\n' "$HOSTSFILE"
+fi
 EOF
 chmod +x /root/malw_parser/malw_parser.sh
 /root/malw_parser/malw_parser.sh
@@ -19,6 +26,5 @@ grep -qF '/root/malw_parser/malw_parser.sh' /etc/crontabs/root 2>/dev/null || \
     echo '0 4 * * * /root/malw_parser/malw_parser.sh' >> /etc/crontabs/root
 
 /etc/init.d/cron restart
-echo "Файл с доменами расположен в ${HOSTSFILE}"
 echo "=== КОНЕЦ! ==="
 echo ""
