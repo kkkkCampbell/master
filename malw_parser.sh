7e6249bb3abdd27cc8dd306398565c5213a3a163
kkkkCampbell <memberof@gmail.com>
2026-09-18 10:07:35 +0300

    Update malw_parser.sh

--- malw_parser.sh
+++ malw_parser.sh
@@ -1,7 +1,7 @@
 mkdir -p /root/malw_parser && tee /root/malw_parser/malw_parser.sh > /dev/null <<'EOF'
 #!/bin/sh
 
-HOSTSFILE=/root/malw_parser/malwhosts.txt
+HOSTSFILE="/root/malw_parser/malwhosts.txt"
 
 # Скачиваем hosts-файл и извлекаем только домены
 wget -qO- "https://raw.githubusercontent.com/ImMALWARE/dns.malw.link/master/hosts" \
@@ -15,6 +15,6 @@ grep -qF '/root/malw_parser/malw_parser.sh' /etc/crontabs/root 2>/dev/null || \
     echo '0 4 * * * /root/malw_parser/malw_parser.sh' >> /etc/crontabs/root
 
 /etc/init.d/cron restart
-echo "Файл с доменами расположен в $HOSTSFILE"
+echo "Файл с доменами расположен в ${HOSTSFILE}"
 echo "=== КОНЕЦ! ==="
 echo ""
