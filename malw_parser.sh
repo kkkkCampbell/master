6503be7b06bb47fc08c3f1ff214c0f133787dd6d
kkkkCampbell <memberof@gmail.com>
2026-09-18 09:55:20 +0300

    Update malw_parser.sh

--- malw_parser.sh
+++ malw_parser.sh
@@ -1,3 +1,4 @@
+mkdir -p /root/malw_parser && tee /root/malw_parser/malw_parser.sh > /dev/null <<'EOF'
 #!/bin/sh
 
 if [ ! -d /root/malw_parser ]; then
@@ -7,4 +8,9 @@ fi
 # Скачиваем hosts-файл и извлекаем только домены
 wget -qO- "https://raw.githubusercontent.com/ImMALWARE/dns.malw.link/master/hosts" \
   | awk '!/^#/ && NF >= 2 { for (i = 2; i <= NF; i++) if ($i ~ /[a-zA-Z]/) print $i }' \
-  > /root/malw_parser/malwhosts.txt	
+  > /root/malw_parser/malwhosts.txt
+EOF
+chmod +x /root/malw_parser/malw_parser.sh
+/root/malw_parser/malw_parser.sh
+
+  
